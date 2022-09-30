20426595780808bd28e4830c97a7300f38bde7a9
Cassius Souza <cassius.sanches@hotmail.com>
2022-09-30 18:44:42 -0300

    feat: add and find user, and check its existence

--- src/repositories/in-memory-user-repository.ts
+++ src/repositories/in-memory-user-repository.ts
@@ -4,20 +4,33 @@ import { UserData } from '../usecases/register-user-on-mailing-list/user-data'
 export class InMemoryUserRepository implements UserRepository {
   private repository: UserData[]
 
-  constructor(repository: UserData[]) {
+  constructor (repository: UserData[]) {
     this.repository = repository
   }
 
-  add(user: UserData): Promise<void> {
-    throw new Error('Method not implemented.')
+  async add (user: UserData): Promise<void> {
+    const exists = await this.exists(user)
+    if (!exists) {
+      this.repository.push(user)
+    }
   }
-  findUserByEmail(email: string): Promise<UserData> {
+
+  async findUserByEmail (email: string): Promise<UserData> {
+    const users = this.repository.filter((user) => user.email === email)
+    if (users.length) {
+      return users[0]
+    }
     return null
   }
-  findAllUsers(): Promise<UserData[]> {
+
+  async findAllUsers (): Promise<UserData[]> {
     throw new Error('Method not implemented.')
   }
-  exists(user: UserData): Promise<boolean> {
-    throw new Error('Method not implemented.')
+
+  async exists (user: UserData): Promise<boolean> {
+    if ((await this.findUserByEmail(user.email)) === null) {
+      return false
+    }
+    return true
   }
 }
