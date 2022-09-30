63812501866259e7f89bde7bfda96ef652cb47ac
Cassius Souza <cassius.sanches@hotmail.com>
2022-09-30 18:47:59 -0300

    test: return null if user not found

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -2,20 +2,12 @@ import { InMemoryUserRepository } from '../../repositories/in-memory-user-reposi
 import { UserData } from './user-data'
 
 describe('Register user on mailing list use case', () => {
-  test('should add user with complete data to mailing list (InMemory)', async () => {
+  test('should return null if user is not found (InMemory)', async () => {
     const users: UserData[] = []
     console.log(users)
-    // const inMemoryUserRepository: UserRepository = new InMemoryUserRepository(
-    //   users
-    // )
-    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(
-    //   inMemoryUserRepository
-    // )
-    // const name = 'any_name'
-    // const email = 'any@email.com'
-    // const response = await useCase.registerUserOnMailingList({ name, email })
-    // const user = inMemoryUserRepository.findUserByEmail(email)
-    // expect((await user).name).toBe(name)
+    const inMemoryUserRepository = new InMemoryUserRepository(users)
+    const user = inMemoryUserRepository.findUserByEmail('fake@email.com')
+    expect(user).toBeNull()
   })
 
   test('should return user if it is found in the repository (InMemory)', async () => {
